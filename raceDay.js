42c7a511cdde2848083ef3a136cc782677cd9209
Nargis <nargis.salim@btinternet.com>
2021-06-27 04:07:09 +0100

    first commit

---
+++ raceDay.js
@@ -0,0 +1,8 @@
+//Race Day
+//Register runners and provide instructions on race day.
+// 9:30 am : Adult and registered early
+// 11:00 am : Adult and registered late
+// 12:30 pm : All youth
+
+const registeredEarly = true;
+const allYouth = true;
