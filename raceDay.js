76642cb13833bc7591821a9abd29db03466137fe
Nargis <nargis.salim@btinternet.com>
2021-06-27 04:24:50 +0100

    update introduction

--- raceDay.js
+++ raceDay.js
@@ -1,5 +1,13 @@
 //Race Day
 //Register runners and provide instructions on race day.
+
+// Runners are assigned a race number and start time based on their age and registration.
+
+// Race number:-
+// Early adults receive a race number at or above 1000.
+// All others receive a number below 1000.
+
+// Start time:-
 // 9:30 am : Adult and registered early
 // 11:00 am : Adult and registered late
 // 12:30 pm : All youth
